d43f3268d0800eecf248ae7a29266aaab4ad4c22
BigsnarfDude <bigsnarfdude@users.noreply.github.com>
2024-01-12 11:00:24 -0700

    Update train.py

--- train.py
+++ train.py
@@ -2,12 +2,20 @@ import pickle
 from alex_net import AlexNet
 import mlx.core as mx
 import mlx.nn as nn
+import mlx.optimizers as optim
+
 
 def unpickle(file):
     with open(file, 'rb') as fo:
         myDict = pickle.load(fo, encoding='latin1')
     return myDict
 
+
+def loss_fn(model, X, y):
+    p = model(X)
+    xe = nn.losses.cross_entropy(p, y)
+    mx.simplify(xe)
+    return mx.mean(xe)
 
 
 def main():
@@ -34,23 +42,27 @@ def main():
     #    print(item, type(trainingData[item]))
     #print(trainingData['data'][0])
 
-    train_images = map(mx.array, trainingData['data'])
-    train_labels = map(mx.array, trainingData['fine_labels'])
+    train_images = map(mx.array, trainingData['data']) # needs split 80-90 percent
+    train_labels = map(mx.array, trainingData['fine_labels']) # needs split 80-90 percent
+    #val
+    #eval 
     
     loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
     optimizer = optim.SGD(learning_rate=learning_rate)
+    print(loss_and_grad_fn)
+    print(optimizer)
 
-    #for e in range(num_epochs):
-    #    tic = time.perf_counter()
-    #    for X, y in batch_iterate(batch_size, train_images, train_labels):
-    #        loss, grads = loss_and_grad_fn(model, X, y)
-    #        optimizer.update(model, grads)
-    #        mx.eval(model.parameters(), optimizer.state)
-    #    accuracy = eval_fn(model, test_images, test_labels)
-    #    toc = time.perf_counter()
-    #    print(
-    #        f"Epoch {e}: Test accuracy {accuracy.item():.3f},"
-    #        f" Time {toc - tic:.3f} (s)"
-    #    )
+    for e in range(num_epochs):
+        tic = time.perf_counter()
+        for X, y in batch_iterate(batch_size, train_images, train_labels):
+            loss, grads = loss_and_grad_fn(model, X, y)
+            optimizer.update(model, grads)
+            mx.eval(model.parameters(), optimizer.state)
+        accuracy = eval_fn(model, test_images, test_labels)
+        toc = time.perf_counter()
+        print(
+            f"Epoch {e}: Test accuracy {accuracy.item():.3f},"
+            f" Time {toc - tic:.3f} (s)"
+        )
     
 main()
